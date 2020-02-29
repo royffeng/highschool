6287cff27f274dce49bc126a0f79907d71744801
royffeng <43551087+royffeng@users.noreply.github.com>
2020-02-29 14:06:59 -0800

    Create Lesson_32_Activity_3.java

---
+++ T1_Lesson_32_Activities/Lesson_32_Activity_3.java
@@ -0,0 +1,15 @@
+class Lesson_32_Activity_Three{
+
+    //Method prints out the swapped
+    public static void swap(int first, int second){
+        System.out.println(second + " " + first);
+    }
+
+    //Main method swaps the values
+    public static void main(String[] args){
+        swap(1, 2);
+        swap(1, 3);
+        swap(1, 4);
+        swap(1, 5);
+    }
+}
