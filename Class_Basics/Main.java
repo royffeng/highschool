5383a62853696ea8e670a1117a31fd93e1ab2ae0
royffeng <43551087+royffeng@users.noreply.github.com>
2020-02-29 13:54:47 -0800

    Create Main.java

---
+++ Class_Basics/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.lang.Math;
+import java.util.*;
+
+class Main {
+  public static void main(String[] args) {
+
+    Employee M = new Employee("Matt the Wii Sports guy", 383929, 17, 85);
+    Employee BC = new Employee("Big Chungus", 983210, 12, 1);
+    Employee D = new Employee("Depression", 111111, 999, 168);
+    Employee S = new Employee("Sadness", 000000, 999, 167);
+
+    System.out.println(M);
+    System.out.println(BC);
+    System.out.println(D);
+    System.out.println(S);
+  }
+}
