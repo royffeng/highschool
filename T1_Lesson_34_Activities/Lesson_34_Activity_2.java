eed3b13164cc5a2b9938460ede1875bad50d0906
royffeng <43551087+royffeng@users.noreply.github.com>
2020-02-29 14:12:10 -0800

    Create Lesson_34_Activity_2.java

---
+++ T1_Lesson_34_Activities/Lesson_34_Activity_2.java
@@ -0,0 +1,18 @@
+class Lesson_34_Activity_Two {
+    public static int sum(int... values) {
+        int result = 0;
+        for (int value : values){
+            result += value;
+        }
+        return result;
+    }
+
+    public static double average(int... a) {
+        return (double) sum(a) / a.length;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(average(1, 2, 3, 4, 5, 6, 7));
+        System.out.println(average(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+    }
+}
